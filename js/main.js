2a3a09030c660b2adebc2169569ddae99bf129cd
JayCogen <jay#wearetmbr.com>
2014-02-26 17:32:28 -0700

    added more trees

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 $(document).ready( function() {
 
-    $('#horiz_png').animationStation({
-    	fps : 12
+
+	setTimeout(function(){
+		$('#lil_tree1').animationStation({
+    		fps : 14
+    	});
+	},500);
+
+    $('#big_tree1').animationStation({
+    	fps : 14
     });
+
+    setTimeout(function(){
+	    $('#lil_tree2').animationStation({
+	    	fps : 14
+	    });
+    },800);
 
 });
